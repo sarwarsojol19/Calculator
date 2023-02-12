503f69980505587329f8109b74963720906b14cf
sarwarsojol19 <sarwarsojol@outlook.com>
2023-02-12 18:26:05 +0600

    all button work is done

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class calculator:
         #====Calculatorrs Buttons====
         
         #====Row01====
+        
         btn_clear=Button(root,text="C",font=("arial",25,"bold"),command=self.clear_cal,cursor="hand2",bd=5,width=5,height=1,bg="#3697f5",fg="#fff")
         btn_clear.place(x=10,y=100)
         
@@ -42,25 +43,43 @@ class calculator:
         btn_sub=Button(root,text="-",font=("arial",25,"bold"),command=lambda:self.get_input('-'),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
         btn_sub.place(x=370,y=180)
         
-        # #====Row03====
+        #====Row03====
         
-        # btn_5=Button(root,text="4",font=("arial",25,"bold"),command=lambda:self.get_input(4),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=3,column=0)
-        # btn_5=Button(root,text="5",font=("arial",25,"bold"),command=lambda:self.get_input(5),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=3,column=1)
-        # btn_6=Button(root,text="6",font=("arial",25,"bold"),command=lambda:self.get_input(6),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=3,column=2)
-        # btn_sub=Button(root,text="-",font=("arial",25,"bold"),command=lambda:self.get_input('-'),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=3,column=3)
+        btn_4=Button(root,text="4",font=("arial",25,"bold"),command=lambda:self.get_input(4),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_4.place(x=10,y=260)
         
-        # #====Row04====
+        btn_5=Button(root,text="5",font=("arial",25,"bold"),command=lambda:self.get_input(5),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_5.place(x=130,y=260)
         
-        # btn_3=Button(root,text="3",font=("arial",25,"bold"),command=lambda:self.get_input(3),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=4,column=0)
-        # btn_2=Button(root,text="2",font=("arial",25,"bold"),command=lambda:self.get_input(2),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=4,column=1)
-        # btn_1=Button(root,text="1",font=("arial",25,"bold"),command=lambda:self.get_input(1),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff").grid(row=4,column=2)
+        btn_6=Button(root,text="6",font=("arial",25,"bold"),command=lambda:self.get_input(6),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_6.place(x=250,y=260)
         
+        btn_sum=Button(root,text="+",font=("arial",25,"bold"),command=lambda:self.get_input('+'),cursor="hand2",bd=5,bg="#2a2d36",fg="#fff",width=5)
+        btn_sum.place(x=370,y=260)
+        
+        #====Row04====
+        
+        btn_1=Button(root,text="1",font=("arial",25,"bold"),command=lambda:self.get_input(1),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_1.place(x=10,y=340)
+        
+        btn_2=Button(root,text="2",font=("arial",25,"bold"),command=lambda:self.get_input(2),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_2.place(x=130,y=340)
+        
+        btn_3=Button(root,text="3",font=("arial",25,"bold"),command=lambda:self.get_input(3),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_3.place(x=250,y=340)
+        
+        btn_eq=Button(root,text="=",font=("arial",20,"bold"),command=self.perform_cal,cursor="hand2",bd=5,width=6,height=3,bg="#fe9037",fg="#fff",pady=16)
+        btn_eq.place(x=370,y=340)
         
         # #====Row05====
         
-        # btn_0=Button(root,text="0",font=("arial",25,"bold"),command=lambda:self.get_input(0),cursor="hand2",bd=5,width=10,bg="#2a2d36",fg="#fff",pady=16).grid(row=5, rowspan=2,column=0)
-        # btn_eq=Button(root,text="=",font=("arial",25,"bold"),command=self.perform_cal,cursor="hand2",bd=5,width=5,bg="#fe9037",fg="#fff",pady=16).grid(row=5,column=2)
-        # btn_sum=Button(root,text="+",font=("arial",25,"bold"),command=lambda:self.get_input('+'),cursor="hand2",bd=5,bg="#2a2d36",fg="#fff",width=5).grid(row=5,column=3)
+        btn_0=Button(root,text="0",font=("arial",25,"bold"),command=lambda:self.get_input(0),cursor="hand2",bd=5,width=11,bg="#2a2d36",fg="#fff")
+        btn_0.place(x=10,y=420)
+        
+        btn_dot=Button(root,text=".",font=("arial",25,"bold"),command=lambda:self.get_input("."),cursor="hand2",bd=5,width=5,bg="#2a2d36",fg="#fff")
+        btn_dot.place(x=250,y=420)
+        
+        
         
     def get_input(self,num):
         xnum=self.var_cal_input.get()+str(num)
